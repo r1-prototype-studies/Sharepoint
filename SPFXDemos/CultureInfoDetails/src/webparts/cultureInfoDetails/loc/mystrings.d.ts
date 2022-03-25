f0c885d437a82f9c7068b81aea309d81c06cf2d7
laptop1 <laptop1@gmail.com>
2022-03-25 13:52:41 +0530

    CultureInfo - Initial

---
+++ SPFXDemos/CultureInfoDetails/src/webparts/cultureInfoDetails/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface ICultureInfoDetailsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'CultureInfoDetailsWebPartStrings' {
+  const strings: ICultureInfoDetailsWebPartStrings;
+  export = strings;
+}
