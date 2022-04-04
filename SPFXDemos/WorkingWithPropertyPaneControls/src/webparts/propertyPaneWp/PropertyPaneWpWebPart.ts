6a40b679d250b53f6168250ca3b9c31b845fa8f0
laptop1 <laptop1@gmail.com>
2022-04-04 14:09:24 +0530

    Property Pane - Adding Render logic

--- SPFXDemos/WorkingWithPropertyPaneControls/src/webparts/propertyPaneWp/PropertyPaneWpWebPart.ts
+++ SPFXDemos/WorkingWithPropertyPaneControls/src/webparts/propertyPaneWp/PropertyPaneWpWebPart.ts
@@ -37,27 +37,37 @@ export default class PropertyPaneWpWebPart extends BaseClientSideWebPart<IProper
   public render(): void {
     this.domElement.innerHTML = `
     <section class="${styles.propertyPaneWp} ${!!this.context.sdks.microsoftTeams ? styles.teams : ''}">
-      <div class="${styles.welcome}">
-        <img alt="" src="${this._isDarkTheme ? require('./assets/welcome-dark.png') : require('./assets/welcome-light.png')}" class="${styles.welcomeImage}" />
-        <h2>Well done, ${escape(this.context.pageContext.user.displayName)}!</h2>
-        <div>${this._environmentMessage}</div>
-        <div>Web part property value: <strong>${escape(this.properties.description)}</strong></div>
-      </div>
-      <div>
-        <h3>Welcome to SharePoint Framework!</h3>
-        <p>
-        The SharePoint Framework (SPFx) is a extensibility model for Microsoft Viva, Microsoft Teams and SharePoint. It's the easiest way to extend Microsoft 365 with automatic Single Sign On, automatic hosting and industry standard tooling.
-        </p>
-        <h4>Learn more about SPFx development:</h4>
-          <ul class="${styles.links}">
-            <li><a href="https://aka.ms/spfx" target="_blank">SharePoint Framework Overview</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-graph" target="_blank">Use Microsoft Graph in your solution</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-teams" target="_blank">Build for Microsoft Teams using SharePoint Framework</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-viva" target="_blank">Build for Microsoft Viva Connections using SharePoint Framework</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-store" target="_blank">Publish SharePoint Framework applications to the marketplace</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-api" target="_blank">SharePoint Framework API reference</a></li>
-            <li><a href="https://aka.ms/m365pnp" target="_blank">Microsoft 365 Developer Community</a></li>
-          </ul>
+      <div class="${styles.propertyPaneWp}">
+        <table>
+          <tr>
+            <td>Product Name</td>
+            <td>${this.properties.productName}</td>
+          </tr>
+          <tr>
+            <td>Description</td>
+            <td>${this.properties.productDescription}</td>
+          </tr>
+          <tr>
+            <td>Product Cost</td>
+            <td>${this.properties.productCost}</td>
+          </tr>
+          <tr>
+            <td>Product Quantity</td>
+            <td>${this.properties.quantity}</td>
+          </tr>
+          <tr>
+            <td>Bill Amount</td>
+            <td>${this.properties.billAmount = this.properties.productCost * this.properties.quantity}</td>
+          </tr>
+          <tr>
+            <td>Discount</td>
+            <td>${this.properties.discount = this.properties.billAmount * 0.1}</td>
+          </tr>
+          <tr>
+            <td>Net Nill Amount</td>
+            <td>${this.properties.netBillAmount = this.properties.billAmount - this.properties.discount}</td>
+          </tr>
+        </table>
       </div>
     </section>`;
   }
