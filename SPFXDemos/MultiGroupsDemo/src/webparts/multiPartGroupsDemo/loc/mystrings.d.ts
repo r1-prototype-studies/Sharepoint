f6d6d8ff864ea98cf37b27ee993c18badc6e24a8
laptop1 <laptop1@gmail.com>
2022-04-07 00:56:34 +0530

    MultiGroup - Initial

---
+++ SPFXDemos/MultiGroupsDemo/src/webparts/multiPartGroupsDemo/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IMultiPartGroupsDemoWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'MultiPartGroupsDemoWebPartStrings' {
+  const strings: IMultiPartGroupsDemoWebPartStrings;
+  export = strings;
+}
