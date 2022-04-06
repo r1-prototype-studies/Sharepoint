2361d9ce090b405756b30af1ed3f08f809c30cd4
laptop1 <laptop1@gmail.com>
2022-04-07 01:21:56 +0530

    Icon - Initial

---
+++ SPFXDemos/IconDemo/src/webparts/testWebPartIcon/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface ITestWebPartIconWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'TestWebPartIconWebPartStrings' {
+  const strings: ITestWebPartIconWebPartStrings;
+  export = strings;
+}
