56e7b29ec07fbb639f3819ae63ecf2ff5618f712
laptop1 <laptop1@gmail.com>
2022-04-07 13:18:31 +0530

    spHttpClient - Creating new list

--- SPFXDemos/ListCreationDemo/src/webparts/newListCreationDemo/NewListCreationDemoWebPart.ts
+++ SPFXDemos/ListCreationDemo/src/webparts/newListCreationDemo/NewListCreationDemoWebPart.ts
@@ -1,23 +1,28 @@
-import { Version } from '@microsoft/sp-core-library';
+import { Version } from "@microsoft/sp-core-library";
 import {
   IPropertyPaneConfiguration,
-  PropertyPaneTextField
-} from '@microsoft/sp-property-pane';
-import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
-import { IReadonlyTheme } from '@microsoft/sp-component-base';
-import { escape } from '@microsoft/sp-lodash-subset';
+  PropertyPaneTextField,
+} from "@microsoft/sp-property-pane";
+import { BaseClientSideWebPart } from "@microsoft/sp-webpart-base";
+import { IReadonlyTheme } from "@microsoft/sp-component-base";
+import { escape } from "@microsoft/sp-lodash-subset";
 
-import styles from './NewListCreationDemoWebPart.module.scss';
-import * as strings from 'NewListCreationDemoWebPartStrings';
+import styles from "./NewListCreationDemoWebPart.module.scss";
+import * as strings from "NewListCreationDemoWebPartStrings";
+
+import {
+  SPHttpClient,
+  SPHttpClientResponse,
+  ISPHttpClientOptions,
+} from "@microsoft/sp-http";
 
 export interface INewListCreationDemoWebPartProps {
   description: string;
 }
 
 export default class NewListCreationDemoWebPart extends BaseClientSideWebPart<INewListCreationDemoWebPartProps> {
-
   private _isDarkTheme: boolean = false;
-  private _environmentMessage: string = '';
+  private _environmentMessage: string = "";
 
   protected onInit(): Promise<void> {
     this._environmentMessage = this._getEnvironmentMessage();
@@ -27,38 +32,87 @@ export default class NewListCreationDemoWebPart extends BaseClientSideWebPart<IN
 
   public render(): void {
     this.domElement.innerHTML = `
-    <section class="${styles.newListCreationDemo} ${!!this.context.sdks.microsoftTeams ? styles.teams : ''}">
-      <div class="${styles.welcome}">
-        <img alt="" src="${this._isDarkTheme ? require('./assets/welcome-dark.png') : require('./assets/welcome-light.png')}" class="${styles.welcomeImage}" />
-        <h2>Well done, ${escape(this.context.pageContext.user.displayName)}!</h2>
-        <div>${this._environmentMessage}</div>
-        <div>Web part property value: <strong>${escape(this.properties.description)}</strong></div>
-      </div>
-      <div>
-        <h3>Welcome to SharePoint Framework!</h3>
-        <p>
-        The SharePoint Framework (SPFx) is a extensibility model for Microsoft Viva, Microsoft Teams and SharePoint. It's the easiest way to extend Microsoft 365 with automatic Single Sign On, automatic hosting and industry standard tooling.
-        </p>
-        <h4>Learn more about SPFx development:</h4>
-          <ul class="${styles.links}">
-            <li><a href="https://aka.ms/spfx" target="_blank">SharePoint Framework Overview</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-graph" target="_blank">Use Microsoft Graph in your solution</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-teams" target="_blank">Build for Microsoft Teams using SharePoint Framework</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-viva" target="_blank">Build for Microsoft Viva Connections using SharePoint Framework</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-store" target="_blank">Publish SharePoint Framework applications to the marketplace</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-api" target="_blank">SharePoint Framework API reference</a></li>
-            <li><a href="https://aka.ms/m365pnp" target="_blank">Microsoft 365 Developer Community</a></li>
-          </ul>
-      </div>
-    </section>`;
+    <div>
+    <h3>Creating a new list dynamically</h3>
+    <br/><br/>
+    <p>Please fill out the below details to create a new list programmatically.</p>
+    <br/>
+    New List Name: <br/><input type="text" id="txtNewListName"/><br/><br/>
+    New List Description: <br/><input type="text" id="txtNewListDescription"/><br/><br/>
+    <input type="button" id="btnCreateNewList" value="Create New List"/><br/><br
+    </div>
+    `;
+    this.bindEvents();
+  }
+
+  private bindEvents(): void {
+    this.domElement
+      .querySelector("#btnCreateNewList")
+      .addEventListener("click", () => {
+        this.createNewList();
+      });
+  }
+
+  private createNewList(): void {
+    var newListName = document.getElementById("txtNewListName")["value"];
+    var newListDescription = document.getElementById("txtNewListDescription")[
+      "value"
+    ];
+
+    const listUrl: string =
+      this.context.pageContext.web.absoluteUrl +
+      "/_api/web/lists/GetByTitle('" +
+      newListName +
+      "')";
+
+    this.context.spHttpClient
+      .get(listUrl, SPHttpClient.configurations.v1)
+      .then((response: SPHttpClientResponse) => {
+        if (response.status === 200) {
+          alert("A list already does exist with this name.");
+          return;
+        }
+        if (response.status === 404) {
+          const url: string =
+            this.context.pageContext.web.absoluteUrl + "/_api/web/lists";
+          const listDefinition: any = {
+            Title: newListName,
+            Description: newListDescription,
+            AllowContentTypes: true,
+            BaseTemplate: 100,
+            ContentTypesEnabled: true,
+          };
+          const spHttpClientOptions: ISPHttpClientOptions = {
+            body: JSON.stringify(listDefinition),
+          };
+          this.context.spHttpClient
+            .post(url, SPHttpClient.configurations.v1, spHttpClientOptions)
+            .then((response: SPHttpClientResponse) => {
+              if (response.status === 201) {
+                alert("A new list is created successfully");
+              } else {
+                alert(
+                  "Error message " + response.status + "-" + response.statusText
+                );
+              }
+            });
+        } else {
+          alert("Error message " + response.status + "-" + response.statusText);
+        }
+      });
   }
 
   private _getEnvironmentMessage(): string {
-    if (!!this.context.sdks.microsoftTeams) { // running in Teams
-      return this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentTeams : strings.AppTeamsTabEnvironment;
+    if (!!this.context.sdks.microsoftTeams) {
+      // running in Teams
+      return this.context.isServedFromLocalhost
+        ? strings.AppLocalEnvironmentTeams
+        : strings.AppTeamsTabEnvironment;
     }
 
-    return this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentSharePoint : strings.AppSharePointEnvironment;
+    return this.context.isServedFromLocalhost
+      ? strings.AppLocalEnvironmentSharePoint
+      : strings.AppSharePointEnvironment;
   }
 
   protected onThemeChanged(currentTheme: IReadonlyTheme | undefined): void {
@@ -67,17 +121,17 @@ export default class NewListCreationDemoWebPart extends BaseClientSideWebPart<IN
     }
 
     this._isDarkTheme = !!currentTheme.isInverted;
-    const {
-      semanticColors
-    } = currentTheme;
-    this.domElement.style.setProperty('--bodyText', semanticColors.bodyText);
-    this.domElement.style.setProperty('--link', semanticColors.link);
-    this.domElement.style.setProperty('--linkHovered', semanticColors.linkHovered);
-
+    const { semanticColors } = currentTheme;
+    this.domElement.style.setProperty("--bodyText", semanticColors.bodyText);
+    this.domElement.style.setProperty("--link", semanticColors.link);
+    this.domElement.style.setProperty(
+      "--linkHovered",
+      semanticColors.linkHovered
+    );
   }
 
   protected get dataVersion(): Version {
-    return Version.parse('1.0');
+    return Version.parse("1.0");
   }
 
   protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
@@ -85,20 +139,20 @@ export default class NewListCreationDemoWebPart extends BaseClientSideWebPart<IN
       pages: [
         {
           header: {
-            description: strings.PropertyPaneDescription
+            description: strings.PropertyPaneDescription,
           },
           groups: [
             {
               groupName: strings.BasicGroupName,
               groupFields: [
-                PropertyPaneTextField('description', {
-                  label: strings.DescriptionFieldLabel
-                })
-              ]
-            }
-          ]
-        }
-      ]
+                PropertyPaneTextField("description", {
+                  label: strings.DescriptionFieldLabel,
+                }),
+              ],
+            },
+          ],
+        },
+      ],
     };
   }
 }
