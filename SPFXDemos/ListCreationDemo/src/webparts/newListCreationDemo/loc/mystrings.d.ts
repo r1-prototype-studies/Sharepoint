a75824ab37e17dc7311144d41652f10026846268
laptop1 <laptop1@gmail.com>
2022-04-07 12:09:22 +0530

    spHttpClient - Initial

---
+++ SPFXDemos/ListCreationDemo/src/webparts/newListCreationDemo/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface INewListCreationDemoWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'NewListCreationDemoWebPartStrings' {
+  const strings: INewListCreationDemoWebPartStrings;
+  export = strings;
+}
