a71d5b416405bc113238147e9a8a5747dccfe5c8
laptop1 <laptop1@gmail.com>
2022-04-07 13:33:18 +0530

    SiteCreation - Initial

---
+++ SPFXDemos/SiteCreationDemo/src/webparts/siteCreationWp/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface ISiteCreationWpWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'SiteCreationWpWebPartStrings' {
+  const strings: ISiteCreationWpWebPartStrings;
+  export = strings;
+}
