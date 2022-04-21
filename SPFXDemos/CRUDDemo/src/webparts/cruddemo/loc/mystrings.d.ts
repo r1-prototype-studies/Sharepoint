4e717bd745c7a234ab2df1c5045425bbb3b45063
laptop1 <laptop1@gmail.com>
2022-04-21 12:46:51 +0530

    CRUD - Initial

---
+++ SPFXDemos/CRUDDemo/src/webparts/cruddemo/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface ICruddemoWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'CruddemoWebPartStrings' {
+  const strings: ICruddemoWebPartStrings;
+  export = strings;
+}
