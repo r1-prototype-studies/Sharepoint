144ff88c8ac2a0ce367e70ac4165421686cfe9b2
laptop1 <laptop1@gmail.com>
2022-04-21 12:37:18 +0530

    SiteCreation - Changes

--- SPFXDemos/SiteCreationDemo/src/webparts/siteCreationWp/SiteCreationWpWebPart.ts
+++ SPFXDemos/SiteCreationDemo/src/webparts/siteCreationWp/SiteCreationWpWebPart.ts
@@ -1,23 +1,28 @@
-import { Version } from '@microsoft/sp-core-library';
+import { Version } from "@microsoft/sp-core-library";
 import {
   IPropertyPaneConfiguration,
-  PropertyPaneTextField
-} from '@microsoft/sp-property-pane';
-import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
-import { IReadonlyTheme } from '@microsoft/sp-component-base';
-import { escape } from '@microsoft/sp-lodash-subset';
+  PropertyPaneTextField,
+} from "@microsoft/sp-property-pane";
+import { BaseClientSideWebPart } from "@microsoft/sp-webpart-base";
+import { IReadonlyTheme } from "@microsoft/sp-component-base";
+import { escape } from "@microsoft/sp-lodash-subset";
 
-import styles from './SiteCreationWpWebPart.module.scss';
-import * as strings from 'SiteCreationWpWebPartStrings';
+import styles from "./SiteCreationWpWebPart.module.scss";
+import * as strings from "SiteCreationWpWebPartStrings";
+
+import {
+  ISPHttpClientOptions,
+  SPHttpClient,
+  SPHttpClientResponse,
+} from "@microsoft/sp-http";
 
 export interface ISiteCreationWpWebPartProps {
   description: string;
 }
 
 export default class SiteCreationWpWebPart extends BaseClientSideWebPart<ISiteCreationWpWebPartProps> {
-
   private _isDarkTheme: boolean = false;
-  private _environmentMessage: string = '';
+  private _environmentMessage: string = "";
 
   protected onInit(): Promise<void> {
     this._environmentMessage = this._getEnvironmentMessage();
@@ -27,38 +32,72 @@ export default class SiteCreationWpWebPart extends BaseClientSideWebPart<ISiteCr
 
   public render(): void {
     this.domElement.innerHTML = `
-    <section class="${styles.siteCreationWp} ${!!this.context.sdks.microsoftTeams ? styles.teams : ''}">
-      <div class="${styles.welcome}">
-        <img alt="" src="${this._isDarkTheme ? require('./assets/welcome-dark.png') : require('./assets/welcome-light.png')}" class="${styles.welcomeImage}" />
-        <h2>Well done, ${escape(this.context.pageContext.user.displayName)}!</h2>
-        <div>${this._environmentMessage}</div>
-        <div>Web part property value: <strong>${escape(this.properties.description)}</strong></div>
-      </div>
-      <div>
-        <h3>Welcome to SharePoint Framework!</h3>
-        <p>
-        The SharePoint Framework (SPFx) is a extensibility model for Microsoft Viva, Microsoft Teams and SharePoint. It's the easiest way to extend Microsoft 365 with automatic Single Sign On, automatic hosting and industry standard tooling.
-        </p>
-        <h4>Learn more about SPFx development:</h4>
-          <ul class="${styles.links}">
-            <li><a href="https://aka.ms/spfx" target="_blank">SharePoint Framework Overview</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-graph" target="_blank">Use Microsoft Graph in your solution</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-teams" target="_blank">Build for Microsoft Teams using SharePoint Framework</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-viva" target="_blank">Build for Microsoft Viva Connections using SharePoint Framework</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-store" target="_blank">Publish SharePoint Framework applications to the marketplace</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-api" target="_blank">SharePoint Framework API reference</a></li>
-            <li><a href="https://aka.ms/m365pnp" target="_blank">Microsoft 365 Developer Community</a></li>
-          </ul>
-      </div>
-    </section>`;
+    <div class="${styles.siteCreationWp}">
+      <h1>Create a new site</h1>
+      <p>Please fill the below details to create a new subsite.</p>
+      <br/>
+      Sub site title: <br/> <input type="text" id="txtSubSiteTitle"/> <br/>
+      Sub site url: <br/> <input type="text" id="txtSubSiteUrl"/> <br/>
+      Sub site description: <br/> <textarea id="txtSubSiteDescription" rows="5" cols="30"></textarea> <br/>
+      <input type="button" id="btnCreateSubSite" value="Create Sub Site"/> <br/>
+    </div>`;
+    this.bindEvents();
+  }
+
+  private bindEvents(): void {
+    this.domElement
+      .querySelector("#btnCreateSubSite")
+      .addEventListener("click", () => {
+        this.createSubSite();
+      });
+  }
+
+  private createSubSite(): void {
+    var subSiteTitle = document.getElementById("txtSubSiteTitle")["value"];
+    var subSiteUrl = document.getElementById("txtSubSiteUrl")["value"];
+    var subSiteDescription = document.getElementById("txtSubSiteDescription")[
+      "value"
+    ];
+
+    const url: string =
+      this.context.pageContext.web.absoluteUrl + "/_api/web/webinfos/add";
+
+    const spHttpClientOptions: ISPHttpClientOptions = {
+      body: `{
+        "parameters": {
+          "@odata.type":"SP.WebInfoCreationInformation",
+          "Title": "${subSiteTitle}",
+          "Url": "${subSiteUrl}",
+          "Description": "${subSiteDescription}",
+          "Language": 1033,
+          "WebTemplate":"STS#0",
+          "UseUniquePermissions": true
+        }
+      }`,
+    };
+
+    this.context.spHttpClient
+      .post(url, SPHttpClient.configurations.v1, spHttpClientOptions)
+      .then((response: SPHttpClientResponse) => {
+        if (response.status === 200) {
+          alert("New Subsite has been created successfully");
+        } else {
+          alert("Error message " + response.status + "-" + response.statusText);
+        }
+      });
   }
 
   private _getEnvironmentMessage(): string {
-    if (!!this.context.sdks.microsoftTeams) { // running in Teams
-      return this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentTeams : strings.AppTeamsTabEnvironment;
+    if (!!this.context.sdks.microsoftTeams) {
+      // running in Teams
+      return this.context.isServedFromLocalhost
+        ? strings.AppLocalEnvironmentTeams
+        : strings.AppTeamsTabEnvironment;
     }
 
-    return this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentSharePoint : strings.AppSharePointEnvironment;
+    return this.context.isServedFromLocalhost
+      ? strings.AppLocalEnvironmentSharePoint
+      : strings.AppSharePointEnvironment;
   }
 
   protected onThemeChanged(currentTheme: IReadonlyTheme | undefined): void {
@@ -67,17 +106,17 @@ export default class SiteCreationWpWebPart extends BaseClientSideWebPart<ISiteCr
     }
 
     this._isDarkTheme = !!currentTheme.isInverted;
-    const {
-      semanticColors
-    } = currentTheme;
-    this.domElement.style.setProperty('--bodyText', semanticColors.bodyText);
-    this.domElement.style.setProperty('--link', semanticColors.link);
-    this.domElement.style.setProperty('--linkHovered', semanticColors.linkHovered);
-
+    const { semanticColors } = currentTheme;
+    this.domElement.style.setProperty("--bodyText", semanticColors.bodyText);
+    this.domElement.style.setProperty("--link", semanticColors.link);
+    this.domElement.style.setProperty(
+      "--linkHovered",
+      semanticColors.linkHovered
+    );
   }
 
   protected get dataVersion(): Version {
-    return Version.parse('1.0');
+    return Version.parse("1.0");
   }
 
   protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
@@ -85,20 +124,20 @@ export default class SiteCreationWpWebPart extends BaseClientSideWebPart<ISiteCr
       pages: [
         {
           header: {
-            description: strings.PropertyPaneDescription
+            description: strings.PropertyPaneDescription,
           },
           groups: [
             {
               groupName: strings.BasicGroupName,
               groupFields: [
-                PropertyPaneTextField('description', {
-                  label: strings.DescriptionFieldLabel
-                })
-              ]
-            }
-          ]
-        }
-      ]
+                PropertyPaneTextField("description", {
+                  label: strings.DescriptionFieldLabel,
+                }),
+              ],
+            },
+          ],
+        },
+      ],
     };
   }
 }
