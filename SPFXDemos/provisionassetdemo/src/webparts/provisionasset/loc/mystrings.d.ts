9df5f30b2e21aa8764e435cdbb34712877c99c8a
laptop1 <laptop1@gmail.com>
2022-04-22 13:44:35 +0530

    Provision assets - Initial

---
+++ SPFXDemos/provisionassetdemo/src/webparts/provisionasset/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IProvisionassetWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'ProvisionassetWebPartStrings' {
+  const strings: IProvisionassetWebPartStrings;
+  export = strings;
+}
