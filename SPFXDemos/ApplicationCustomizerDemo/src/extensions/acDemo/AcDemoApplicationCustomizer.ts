7f3a1a5e3e24c7578de508bdb4f5131b3de7024c
laptop1 <laptop1@gmail.com>
2022-05-03 20:15:15 +0530

    Application Customization Extension - Header and Footer

--- SPFXDemos/ApplicationCustomizerDemo/src/extensions/acDemo/AcDemoApplicationCustomizer.ts
+++ SPFXDemos/ApplicationCustomizerDemo/src/extensions/acDemo/AcDemoApplicationCustomizer.ts
@@ -1,5 +1,14 @@
 import { Log } from "@microsoft/sp-core-library";
-import { BaseApplicationCustomizer } from "@microsoft/sp-application-base";
+import {
+  BaseApplicationCustomizer,
+  PlaceholderContent,
+  PlaceholderName,
+} from "@microsoft/sp-application-base";
+
+import { escape } from "@microsoft/sp-lodash-subset";
+
+import styles from "./ACDemo.module.scss";
+
 import { Dialog } from "@microsoft/sp-dialog";
 
 import * as strings from "AcDemoApplicationCustomizerStrings";
@@ -13,22 +22,103 @@ const LOG_SOURCE: string = "AcDemoApplicationCustomizer";
  */
 export interface IAcDemoApplicationCustomizerProperties {
   // This is an example; replace with your own property
-  testMessage: string;
+  Top: string;
+  Bottom: string;
 }
 
 /** A Custom Action which can be run during execution of a Client Side Application */
 export default class AcDemoApplicationCustomizer extends BaseApplicationCustomizer<IAcDemoApplicationCustomizerProperties> {
+  private _topPlaceholder: PlaceholderContent | undefined;
+  private _bottomPlaceholder: PlaceholderContent | undefined;
+
   public onInit(): Promise<void> {
     Log.info(LOG_SOURCE, `Initialized ${strings.Title}`);
 
-    let message: string = this.properties.testMessage;
+    this.context.placeholderProvider.changedEvent.add(
+      this,
+      this._renderPlaceHolders
+    );
 
-    if (!message) {
-      message = "(No properties were provided.)";
-    }
-
-    Dialog.alert(`Hello from ${strings.Title}:\n\n${message}`);
+    this._renderPlaceHolders();
 
     return Promise.resolve();
+  }
+
+  private _renderPlaceHolders(): void {
+    console.log(
+      "Available placeholders are : ",
+      this.context.placeholderProvider.placeholderNames
+        .map((placeholdername) => PlaceholderName[placeholdername])
+        .join(", ")
+    );
+
+    if (!this._topPlaceholder) {
+      this._topPlaceholder = this.context.placeholderProvider.tryCreateContent(
+        PlaceholderName.Top,
+        { onDispose: this._onDispose }
+      );
+
+      if (!this._topPlaceholder) {
+        console.error("The placeholder Top was not found...");
+        return;
+      }
+
+      if (this.properties) {
+        let topString: string = this.properties.Top;
+        if (!topString) {
+          topString = "(Top property was not defined...)";
+        }
+
+        if (this._topPlaceholder.domElement) {
+          this._topPlaceholder.domElement.innerHTML = `
+            <div class="${styles.acdemoapp}">
+              <div class="ms-bgColor-themeDark ms-fontColor-white ${
+                styles.topPlaceholder
+              }">
+                <i class="ms-Icon ms-Icon--Info" aria-hidden="true"></i> ${escape(
+                  topString
+                )}
+              </div>
+            </div>`;
+        }
+      }
+    }
+
+    if (!this._bottomPlaceholder) {
+      this._bottomPlaceholder =
+        this.context.placeholderProvider.tryCreateContent(
+          PlaceholderName.Bottom,
+          { onDispose: this._onDispose }
+        );
+
+      if (!this._bottomPlaceholder) {
+        console.error("The placeholder Bottom was not found...");
+        return;
+      }
+
+      if (this.properties) {
+        let bottomString: string = this.properties.Bottom;
+        if (!bottomString) {
+          bottomString = "(Bottom property was not defined...)";
+        }
+
+        if (this._bottomPlaceholder.domElement) {
+          this._bottomPlaceholder.domElement.innerHTML = `
+            <div class="${styles.acdemoapp}">
+              <div class="ms-bgColor-themeDark ms-fontColor-white ${
+                styles.bottomPlaceholder
+              }">
+                <i class="ms-Icon ms-Icon--Info" aria-hidden="true"></i> ${escape(
+                  bottomString
+                )}
+              </div>
+            </div>`;
+        }
+      }
+    }
+  }
+
+  private _onDispose(): void {
+    console.log("Disposed custom top and bottom placeholders");
   }
 }
