c2b2978427f41db4eafd574e360031aa28367675
laptop1 <laptop1@gmail.com>
2022-05-04 11:11:50 +0530

    React lifecycle - Initial

---
+++ SPFXDemos/ReactLifecycleDemo/src/webparts/reactLifeCycleWebPart/components/IReactLifeCycleWebPartProps.ts
@@ -0,0 +1,7 @@
+export interface IReactLifeCycleWebPartProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ SPFXDemos/ReactLifecycleDemo/src/webparts/reactLifeCycleWebPart/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IReactLifeCycleWebPartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'ReactLifeCycleWebPartWebPartStrings' {
+  const strings: IReactLifeCycleWebPartWebPartStrings;
+  export = strings;
+}
