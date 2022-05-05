9df274d82b8536282b2cc18b03205f17c2efe429
laptop1 <laptop1@gmail.com>
2022-05-05 12:54:01 +0530

    Anonymous API - Initial

---
+++ SPFXDemos/AnonymousAPIDemo2/src/webparts/anonymousApi/components/IAnonymousApiProps.ts
@@ -0,0 +1,7 @@
+export interface IAnonymousApiProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ SPFXDemos/AnonymousAPIDemo2/src/webparts/anonymousApi/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IAnonymousApiWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'AnonymousApiWebPartStrings' {
+  const strings: IAnonymousApiWebPartStrings;
+  export = strings;
+}
