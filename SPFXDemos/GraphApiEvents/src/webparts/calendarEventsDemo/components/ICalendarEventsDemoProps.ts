ee0227c257991617be5938f55edf17e891480692
laptop1 <laptop1@gmail.com>
2022-05-06 09:24:37 +0530

    Sharepoint Calendar - Initial

---
+++ SPFXDemos/GraphApiEvents/src/webparts/calendarEventsDemo/components/ICalendarEventsDemoProps.ts
@@ -0,0 +1,7 @@
+export interface ICalendarEventsDemoProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ SPFXDemos/GraphApiEvents/src/webparts/calendarEventsDemo/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface ICalendarEventsDemoWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'CalendarEventsDemoWebPartStrings' {
+  const strings: ICalendarEventsDemoWebPartStrings;
+  export = strings;
+}
